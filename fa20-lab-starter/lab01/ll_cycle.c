64cb248e2cce75c67b778a3f32c0ea5680a3a4b4
happypie <nkelvin1116@gmail.com>
2024-04-15 12:26:22 -0400

    completed exercises 3, 4, 5 and finished lab

--- fa20-lab-starter/lab01/ll_cycle.c
+++ fa20-lab-starter/lab01/ll_cycle.c
@@ -1,7 +1,27 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
+
+// No Cycle is 0
+// Cycle is 1
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node* tortoise = head;
+    node* hare = head;
+
+    while (hare != NULL){
+        hare = (*hare).next;
+        tortoise = (*tortoise).next;
+
+        if (hare != NULL){
+            hare = (*hare).next;
+        }
+
+        if (hare == tortoise){
+            if (hare == NULL){
+                return 0;
+            }
+            return 1;
+        }
+    }
     return 0;
 }
--- fa20-lab-starter/lab01/segfault_ex.c
+++ fa20-lab-starter/lab01/segfault_ex.c
@@ -2,6 +2,5 @@ int main() {
     int a[20];
     for (int i = 0; ; i++) {
         a[i] = i;
-        printf("%d", i);
     }
 }
