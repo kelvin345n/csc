34a085acf5969b06b366bb3294a3de7d54834b84
kelvin345n <nkelvin1116@gmail.com>
2024-04-25 16:21:02 -0400

    Tweaks to factorial.s

--- fa20-lab-starter/lab03/factorial.s
+++ fa20-lab-starter/lab03/factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 7
+n: .word 8
 
 .text
 main:
@@ -23,23 +23,26 @@ main:
 factorial:
     addi t0, x0, 2 # If argument is 1 or 0 then return 
     blt a0, t0, return
-
+    
+    #BEGIN PROLOGUE
     addi sp, sp, -8 # Increase Stack by a word
     sw s0, 0(sp)    # Store s0 onto the stack to restore later
     sw ra, 4(sp)    # Store the return address
     mv s0, a0       # Copy the argument into s0 because we will use recursion
+    #END PROLOGUE
     
-    addi a0, a0, -1 # Decrement the arg by 1 and recursively call factorial
+    addi a0, a0, -1 
     jal factorial
     mul s0, s0, a0 # a0 is not another call from factorial
     mv a0, s0       # Return the factorial in a0
     
+    #BEGIN EPILOGUE
     lw s0, 0(sp)    # Restoring values
     lw ra, 4(sp)
     addi sp, sp, 8 
+    #END EPILOGUE
     
     jr ra
-    
     
 return:
     addi a0, x0 1 # argument is 1 or 0 so we return 1
