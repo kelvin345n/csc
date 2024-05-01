942022d635e9b065fc14ac574028dc60a9380cf8
kelvin345n <nkelvin1116@gmail.com>
2024-05-01 13:15:12 -0400

    Fixed argmax to now deal with zero vectors

--- fa20_projects_61c/fa20-proj2-starter/src/argmax.s
+++ fa20_projects_61c/fa20-proj2-starter/src/argmax.s
@@ -15,20 +15,42 @@
 #   this function terminates the program with error code 77.
 # =================================================================
 argmax:
-
-    # Prologue
-
-
-loop_start:
-
-
-loop_continue:
-
-
-loop_end:
+    beq a1, x0, loop_end        
     
-
-    # Epilogue
-
-
+    mv t0, a0          # t0 stores the address of array
+    mv t1, a1          # t1 stores the num of elems in array
+    li t2, 0           # t2 stores the current counter
+                       # t3 stores the address of current element
+    li t4, 0           # t4 stores the index of the max elem so far. Initialized to 0
+    li a5, 0           # a5 stores the sum of squares of all the elems
+                       # a6 stores the value of the current elem.
+    lw a7, 0(t0)       # a7 stores the value of the max elem. Initialied to elem at index 0
+loop_start:
+    beq t1, t2, load_return     # end loop if counter == num of elems in array
+    addi t5, x0, 4              # t5 stores the amount of bytes in an integer
+    mul t6, t5, t2              # t6 stores amount of bytes until next elem
+    add t3, t0, t6          
+    lw a6, 0(t3)
+    
+    mul a4, a6, a6          # Square of the current elem
+    add a5, a5, a4          # Add to total sum
+    
+    beq a6, a7, loop_continue
+    blt a6, a7, loop_continue
+    # means the current element is larger than the reigning champ
+    mv t4, t2
+    mv a7, a6
+loop_continue:  
+    addi t2, t2, 1
+    j loop_start
+    
+load_return:
+    mv a0, t4           # Stores index of max elem to return
+loop_end:
+    beq a5, x0, error  # If vector is 0, then return error code 77
     ret
+ 
+error:
+    li a0, 17
+    li a1, 77
+    ecall
