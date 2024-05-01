9b224b45a570ecf3ccc665a9ec33aaa36750dda9
kelvin345n <nkelvin1116@gmail.com>
2024-05-01 13:15:29 -0400

    Task2-Proj2

--- fa20_projects_61c/fa20-proj2-starter/src/relu.s
+++ fa20_projects_61c/fa20-proj2-starter/src/relu.s
@@ -15,17 +15,20 @@
 # ==============================================================================
 relu:
     # Prologue
-    addi sp, sp, -24
+    addi sp, sp, -28
     sw ra, 0(sp)
     sw s0, 4(sp)        # s0 stores the pointer to the array. 
     sw s1, 8(sp)        # s1 stores the num of elems in array
     sw s2, 12(sp)       # s2 stores the current counter. Intialized to 0
     sw s3, 16(sp)       # s3 stores the address of the current elem.
-    sw s4, 20(sp)       # 24 stores the value of the current elem. 
+    sw s4, 20(sp)       # s4 stores the value of the current elem. 
+    sw s5, 24(sp)       # s5 stores the psuedo-length of the vector. Just the sum of all
+                        # the elems    
     # Prologue
     mv s0, a0
     mv s1, a1
     addi s2, x0, 0
+    addi s5, x0, 0
 loop_start:
     beq s2, s1, loop_end     # If counter is equal to the number of elems in array; exit
     addi t0, x0, 4           # t0 stores the amount of bytes in an integer
@@ -34,22 +37,7 @@ loop_start:
     lw s4, 0(s3)             # s4 holds the value of the integer
     mv a0, s4                # Load arg for the abs function
     jal abs                  # a0 holds the absolute value of the integer
-    
-    # If absolute value of vector is less than 1: error. 
-    addi t0, x0, 1
-    bge a0, t0, loop_continue
-    li a0, 17
-    li a1, 78
-    # Epilogue
-    lw ra, 0(sp)
-    lw s0, 4(sp)     
-    lw s1, 8(sp)    
-    lw s2, 12(sp)
-    lw s3, 16(sp)    
-    lw s4, 20(sp)
-    addi sp, sp, 24
-    # Epilogue
-    ecall
+    add s5, s5, a0
 loop_continue:
     addi t0, x0, 0
     bge s4, t0, big         # Keep the number the same if the integer is greater than 0.
@@ -57,7 +45,9 @@ loop_continue:
 big: 
     addi s2, s2, 1          # Increment counter
     j loop_start
+
 loop_end:
+    mv t0, s5       # Move sum to temp register      
     # Epilogue
     lw ra, 0(sp)
     lw s0, 4(sp)     
@@ -65,6 +55,13 @@ loop_end:
     lw s2, 12(sp)
     lw s3, 16(sp)    
     lw s4, 20(sp)
-    addi sp, sp, 24
+    lw s5, 24(sp)
+    addi sp, sp, 28
     # Epilogue
+    li t1, 1
+    blt t0, t1, error   # If length of psuedo length is less than 1: error
 	ret
+error:      # return error code 78 if length of vector is less than one
+    li a0, 17
+    li a1, 78
+    ecall
--- fa20_projects_61c/fa20-proj2-starter/unittests/unittests.py
+++ fa20_projects_61c/fa20-proj2-starter/unittests/unittests.py
@@ -97,7 +97,20 @@ class TestRelu(TestCase):
     def test_zero_vector(self):
         t = AssemblyTest(self, "relu.s")
         # create an array in the data section
-        array0 = t.array([0])
+        array0 = t.array([0, 0, 0, 0])
+        # load address of `array0` into register a0
+        t.input_array("a0", array0)
+        # set a1 to the length of our array
+        t.input_scalar("a1", len(array0))
+        # call the relu function
+        t.call("relu")
+        # generate the `assembly/TestRelu_test_simple.s` file and run it through venus
+        t.execute(code=78)
+        
+    def test_zero_more_vector(self):
+        t = AssemblyTest(self, "relu.s")
+        # create an array in the data section
+        array0 = t.array([0, 0, 0, 0, -1, 0, 0])
         # load address of `array0` into register a0
         t.input_array("a0", array0)
         # set a1 to the length of our array
@@ -105,31 +118,101 @@ class TestRelu(TestCase):
         # call the relu function
         t.call("relu")
         # check that the array0 was changed appropriately
-        t.check_array(array0, [0])
+        t.check_array(array0, [0, 0, 0, 0, 0, 0, 0])
         # generate the `assembly/TestRelu_test_simple.s` file and run it through venus
-        t.execute(code=78)
+        t.execute()
 
     @classmethod
     def tearDownClass(cls):
         print_coverage("relu.s", verbose=False)
 
-
 class TestArgmax(TestCase):
     def test_simple(self):
         t = AssemblyTest(self, "argmax.s")
         # create an array in the data section
-        raise NotImplementedError("TODO")
-        # TODO
+        array0 = t.array([1, -2, 3, -4, 5, -6, 7, -8, 9])
         # load address of the array into register a0
-        # TODO
-        # set a1 to the length of the array
-        # TODO
+        t.input_array("a0", array0)
+        # set a1 to the length of our array
+        t.input_scalar("a1", len(array0))
         # call the `argmax` function
-        # TODO
+        t.call("argmax")
         # check that the register a0 contains the correct output
-        # TODO
+        t.check_scalar("a0", 8)
         # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
         t.execute()
+
+    def test_duplicates(self):
+        t = AssemblyTest(self, "argmax.s")
+        # create an array in the data section
+        array0 = t.array([1, 1, 3, 3, 3, 3, 2, 2, 2])
+        # load address of the array into register a0
+        t.input_array("a0", array0)
+        # set a1 to the length of our array
+        t.input_scalar("a1", len(array0))
+        # call the `argmax` function
+        t.call("argmax")
+        # check that the register a0 contains the correct output
+        t.check_scalar("a0", 2)
+        # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
+        t.execute()
+
+    def test_one_elem(self):
+        t = AssemblyTest(self, "argmax.s")
+        # create an array in the data section
+        array0 = t.array([1])
+        # load address of the array into register a0
+        t.input_array("a0", array0)
+        # set a1 to the length of our array
+        t.input_scalar("a1", len(array0))
+        # call the `argmax` function
+        t.call("argmax")
+        # check that the register a0 contains the correct output
+        t.check_scalar("a0", 0)
+        # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
+        t.execute()
+
+    def test_negatives(self):
+        t = AssemblyTest(self, "argmax.s")
+        # create an array in the data section
+        array0 = t.array([-10, -11, -3, -3, -1, -3, -2, -2, -2])
+        # load address of the array into register a0
+        t.input_array("a0", array0)
+        # set a1 to the length of our array
+        t.input_scalar("a1", len(array0))
+        # call the `argmax` function
+        t.call("argmax")
+        # check that the register a0 contains the correct output
+        t.check_scalar("a0", 4)
+        # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
+        t.execute()
+        
+    def test_zero_vector(self):
+        t = AssemblyTest(self, "argmax.s")
+        # create an array in the data section
+        array0 = t.array([0, 0, 0, 0, 0, 0])
+        # load address of the array into register a0
+        t.input_array("a0", array0)
+        # set a1 to the length of our array
+        t.input_scalar("a1", len(array0))
+        # call the `argmax` function
+        t.call("argmax")
+        # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
+        t.execute(code=77)
+    
+    def test_zero_vector2(self):
+        t = AssemblyTest(self, "argmax.s")
+        # create an array in the data section
+        array0 = t.array([0])
+        # load address of the array into register a0
+        t.input_array("a0", array0)
+        # set a1 to the length of our array
+        t.input_scalar("a1", len(array0))
+        # call the `argmax` function
+        t.call("argmax")
+        # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
+        t.execute(code=77)
+        
 
     @classmethod
     def tearDownClass(cls):
